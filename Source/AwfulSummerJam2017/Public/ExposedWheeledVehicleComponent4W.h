0a0a4c791361fd162b66e1cc9ba8ad1e5c0e00d4
Praseodymi <Praseodymi@gmail.com>
2017-07-29 14:39:45 +0100

    sort out CivCar->PoliceCar heirarchy

---
+++ Source/AwfulSummerJam2017/Public/ExposedWheeledVehicleComponent4W.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehicleMovementComponent4W.h"
+#include "ExposedWheeledVehicleComponent4W.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AWFULSUMMERJAM2017_API UExposedWheeledVehicleComponent4W : public UWheeledVehicleMovementComponent4W
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
